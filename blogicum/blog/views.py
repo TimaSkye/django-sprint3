9cdb2dfdf8b4763272bb6d0fd5b98bfa56c5b10c
Тимур Даянов <tima.skye@yandex.ru>
2025-04-15 21:19:16 +0500

    подготовка перед редактированием view и templates.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,6 +2,9 @@ from django.http import Http404
 from django.shortcuts import render
 
 
+# posts_dict: dict[int, dict[str, str]] = {post['id']: post for post in posts}
+
+
 def index(request) -> None:
     """Функция рендера главной страницы проекта."""
     return render(request, 'blog/index.html', {'posts': posts[::-1]})
@@ -17,5 +20,4 @@ def post_detail(request, post_id: int) -> None:
 
 def category_posts(request, category_slug: str) -> None:
     """Функция рендера страницы категорий поста."""
-    return render(request, 'blog/category.html',
-                  {'category_posts': category_slug})
+    return render(request, 'blog/category.html', {'category_posts': category_slug})
