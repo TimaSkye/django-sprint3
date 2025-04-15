9b4dc9dfe279b2ef1d05c422076f90d5e8b4a14e
Тимур Даянов <tima.skye@yandex.ru>
2025-04-15 20:47:19 +0500

    Настройка админ панели. Удаление templates.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from .models import Category, Location, Post
+from core.admin import CategoryAdmin, LocationAdmin, PostAdmin
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+admin.site.empty_value_display = 'Не задано'
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Post, PostAdmin)
--- blogicum/core/admin.py
+++ blogicum/core/admin.py
@@ -1,6 +1,58 @@
 from django.contrib import admin
 
 
-class BlogAdmin(admin.ModelAdmin):
-    """Переопределение админ-панели приложения Blog."""
-    pass
+class CategoryAdmin(admin.ModelAdmin):
+    """Настройка админ-панели модели Категории."""
+
+    list_display = ('title', 'description', 'slug', 'is_published', 'created_at')
+    list_editable = ('description', 'slug', 'is_published')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    list_display_links = ('title',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    """Настройка админ-панели модели Локации."""
+
+    list_display = ('name', 'is_published', 'created_at')
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    list_display_links = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Настройка админ-панели модели Публикации."""
+
+    list_display = (
+        'title',
+        'text',
+        'pub_date',
+        'author',
+        'location',
+        'category',
+        'is_published',
+        'created_at',
+    )
+    list_editable = (
+        'text',
+        'pub_date',
+        'author',
+        'location',
+        'category',
+        'is_published',
+    )
+    search_fields = (
+        'title',
+        'author',
+        'location',
+        'category',
+    )
+    list_filter = (
+        'title',
+        'pub_date',
+        'author',
+        'location',
+        'category',
+    )
+    list_display_links = ('title',)
